aa50e60b93c641a3238af984444a6e2ef0ce4c5a
Huang Jian-Min <yfr.huang@hotmail.com>
2020-03-08 01:24:49 +0800

    feat: First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import com.slack.api.bolt.App;
+
+import com.slack.api.bolt.jetty.SlackAppServer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // setup slack app
+        // App expects env variables (SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET)
+        App app = new App();
+
+        // define handler
+        app.command("/hello", (req, ctx) -> {
+            return ctx.ack(":wave: Hello!");
+        });
+
+        // listen and start
+        SlackAppServer server = new SlackAppServer(app);
+        server.start(); // http://localhost:3000/slack/events
+    }
+
+}
